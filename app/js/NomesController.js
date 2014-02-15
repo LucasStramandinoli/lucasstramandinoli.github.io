9b58f001a8342c24d105ab4446aa2853cc064bf0
LucasStramandinoli <lukasmartins@hotmail.com>
2014-02-15 20:08:12 -0200

    Worked
    uploading all files

---
+++ app/js/NomesController.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var NomesApp = angular.module('NomesApp', []);
+
+NomesApp.controller('NomesListCtrl',function ($scope){
+    
+   $scope.nomes = [
+       {'name':'Lucas','sobrenome':'Stramandinoli'},
+       {'name':'Junior','sobrenome':'Soares'}
+   ];
+   
+   $scope.AddNome = function (){
+     $scope.nomes.push(
+             {name:$scope.item.name,sobrenome:$scope.item.name}
+               );
+   };
+   
+});
---
+++ app/js/tabelaController.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var phonecatApp = angular.module('phonecatApp', []);
+ 
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
